2a6aec92bcb681d3666e2b18ea3a557d1c3f7827
deepakdeep123 <bhattacharya.deep@gmail.com>
2017-04-05 13:06:30 +0530

    Add files via upload

---
+++ app6.2.js
@@ -0,0 +1,23 @@
+
+var a=10,b=100,c=70;
+
+/* checks a>b and a>c  if  both conditions satisfied, A is greater */
+
+if (a>b  && a>c)
+{
+console.log("<b>A is greater</b>");
+}
+
+/* checks b>a  and b>c  if  both conditions satisfied, b is greater */
+
+if (b>a  && b>c)
+{
+console.log("<b>B is greater</b>");
+}
+
+/* if the above two conditions were false c is greater*/
+
+else
+{
+console.log("<b>C is greater</b>");
+}
